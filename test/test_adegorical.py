acf2500dbd361915bfbd6d9f751b903d043f5e7b
joshua.bragge@hotmail.com <joshuabragge@hotmail.com>
2018-09-15 17:29:41 -0600

    added initial dummy tests, created test data

--- test/test_adegorical.py
+++ test/test_adegorical.py
@@ -4,8 +4,30 @@ import pandas as pd
 import numpy as np
 from adegorical import adegorical as ad
 
+small_unique = 10
+medium_unique = 40
+large_unique = 100
+insane_unique = 1000
+
+count_multiplier = 10
+
+categorical_data_simple = ['yellow', 'red', 'yellow','red', 'magenta']
+categorical_data_small_unique_small_count = [str(x) for x in range(small_unique)] + [str(x*2) for x in range(int(small_unique/2))] + [str(x*3) for x in reversed(range(int(small_unique/3)))]
+categorical_data_medium_unique_small_count = [str(x) for x in range(medium_unique)] + [str(x*2) for x in range(int(medium_unique/2))] + [str(x*3) for x in reversed(range(int(medium_unique/3)))]
+categorical_data_large_unique_small_count = [str(x) for x in range(large_unique)] + [str(x*2) for x in range(int(large_unique/2))] + [str(x*3) for x in reversed(range(int(large_unique/3)))]
+
+categorical_data_small_unique_large_count = categorical_data_small_unique_small_count * count_multiplier
+categorical_data_medium_unique_large_count = categorical_data_medium_unique_small_count * count_multiplier
+categorical_data_large_unique_large_count = categorical_data_large_unique_small_count * count_multiplier
+
+test_datasets = [categorical_data_small_unique_large_count, categorical_data_medium_unique_large_count, categorical_data_large_unique_large_count,
+				 categorical_data_small_unique_small_count, categorical_data_medium_unique_small_count, categorical_data_large_unique_small_count]
+
 
 class Help(unittest.TestCase):
+	"""
+	Ensures the help function has all the encoding methods available
+	"""
 
 	encoding_types = ['dummy', 'binary', 'simple_contrast', 'simple_regression', 'backward_difference_contrast', 'forward_difference_contrast', 'simple_helmert']
 
@@ -15,30 +37,72 @@ class Help(unittest.TestCase):
 
 
 class DataType(unittest.TestCase):
-	categorical_data = ['yellow', 'red', 'yellow','red', 'magenta']
+	"""
+	Checks that methods return the data type given 
+	for all encoding methods
+	"""
+	
 	encoding_methods = ad.help()
 
 	def test_pandas_series_output(self):
-		'''checks that pandas series is outputed'''
-		pandas_series = pd.Series(self.categorical_data)
+		pandas_series = pd.Series(categorical_data_simple)
 		for encoding_method in self.encoding_methods:
 			encoded_results_pandas = ad.get_categorical(pandas_series, encoding=encoding_method)
 			self.assertTrue(isinstance(encoded_results_pandas, pd.DataFrame))
 
 	def test_numpy_array_output(self):
-		'''checks that numpy ndarray is outputed'''
-		numpy_array = np.array(self.categorical_data)
+		numpy_array = np.array(categorical_data_simple)
 		for encoding_method in self.encoding_methods:
 			encoded_results_numpy = ad.get_categorical(numpy_array, encoding=encoding_method)
 			self.assertTrue(isinstance(encoded_results_numpy, np.ndarray))
 				
 
 	def test_list_output(self):
-		'''checks that list is outputed'''
-		python_list = self.categorical_data
+		python_list = categorical_data_simple
 		for encoding_method in self.encoding_methods:
 			encoded_results_list = ad.get_categorical(python_list, encoding=encoding_method)	
 			self.assertTrue(isinstance(encoded_results_list, list))
+
+
+class DummyEncoding(unittest.TestCase):
+	"""
+	Test cases for dummy variable encoding.
+	Checks that the correct number of columns are returned for dummy encoded objects. 
+	n-1 columns should be returned where n is the number of unique variables.
+	"""
+	def test_dummy_list_column_size(self):
+
+		for test_dataset in test_datasets:
+			unique_variables = len(set(test_dataset))
+			test_dataset_column_count = unique_variables - 1
+			encoded_results_column_count = len(ad.get_categorical(test_dataset, encoding='dummy')[0])
+			self.assertEqual(test_dataset_column_count, encoded_results_column_count)
+
+	def test_dummy_pandas_column_size(self):
+
+		for test_dataset in test_datasets:
+			unique_variables = len(set(test_dataset))
+			test_dataset_column_count = unique_variables - 1
+			test_dataset_pandas = pd.Series(test_dataset)
+			encoded_results_column_count = ad.get_categorical(test_dataset_pandas, encoding='dummy').shape[1]
+			self.assertEqual(test_dataset_column_count, encoded_results_column_count)
+
+	def test_dummy_numpy_column_size(self):
+
+		for test_dataset in test_datasets:
+			unique_variables = len(set(test_dataset))
+			test_dataset_column_count = unique_variables - 1
+			test_dataset_numpy = np.array(test_dataset)
+			encoded_results_column_count = len(ad.get_categorical(test_dataset_numpy, encoding='dummy')[0])
+			self.assertEqual(test_dataset_column_count, encoded_results_column_count)
+
+	def test_dummy_zero_len_dataset(self):
+		encoding_methods = ad.help()
+		zero_dataset = []
+
+		for encoding_method in self.encoding_methods:
+			encoded_results_numpy = ad.get_categorical(numpy_array, encoding=encoding_method)
+			self.assertTrue(isinstance(encoded_results_numpy, np.ndarray))
 
 
 if __name__ == '__main__':
